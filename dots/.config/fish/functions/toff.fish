c3250f8e2b67049dbb9069ef5a1a96d5133db7d6
djOB2EOTWQW1 <uC7TQ5A1b9xRZV8mE2@proton.me>
2026-04-18 18:09:55 +0300

    delete my files for fish

--- dots/.config/fish/functions/toff.fish
+++ dots/.config/fish/functions/toff.fish
@@ -1,7 +1,9 @@
 #!/usr/bin/env fish
-function toff -d "Disable touchpad"
-    hyprctl keyword "device[pnp0c50:00-04f3:30aa-touchpad]:enabled false"
-    hyprctl keyword "device[etps/2-elantech-touchpad]:enabled false"
+function toff -d "Disable touchpad and restore mouse sensitivity -1"
+    hyprctl keyword "device[pnp0c50:00-04f3:30aa-touchpad]:enabled" false
+    hyprctl keyword "device[etps/2-elantech-touchpad]:enabled" false
 
-    echo "Touchpad disabled."
+    hyprctl keyword input:sensitivity -1
+
+    echo "Touchpad disabled. Global sensitivity restored to -1"
 end
--- dots/.config/fish/functions/ton.fish
+++ dots/.config/fish/functions/ton.fish
@@ -1,7 +1,12 @@
 #!/usr/bin/env fish
-function ton -d "Enable touchpad"
-    hyprctl keyword "device[pnp0c50:00-04f3:30aa-touchpad]:enabled true"
-    hyprctl keyword "device[etps/2-elantech-touchpad]:enabled true"
+function ton -d "Enable touchpad with good sensitivity"
+    hyprctl keyword "device[pnp0c50:00-04f3:30aa-touchpad]:enabled" true
+    hyprctl keyword "device[etps/2-elantech-touchpad]:enabled" true
 
-    echo "Touchpad enabled."
+    sleep 0.25
+
+    hyprctl keyword "device[pnp0c50:00-04f3:30aa-touchpad]:sensitivity" -0.4
+    hyprctl keyword "device[etps/2-elantech-touchpad]:sensitivity" -0.4
+
+    echo "Touchpad enabled (sensitivity 0.0). Mouse stays at -1"
 end
